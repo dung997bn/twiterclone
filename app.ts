358c8d22a069151012f3ba90b87e0432f62618a1
dung997bn <phaichisophan@gmail.com>
2020-11-21 12:45:55 +0700

    first commit

---
+++ app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import { NextFunction, Request, Response } from 'express';
+import pug from 'pug'
+const app = express()
+const port = 3003
+
+const server = app.listen(port, () => {
+    console.log("Server listening on port " + port);
+})
+
+app.set("view engine", "pug")
+app.set("views", "views")
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+
+    let payload ={
+        pageTitle:' Home'
+    }
+    res.status(200).render("home",payload)
+})
